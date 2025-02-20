b0fb6c68e5e9298d28418c05daf57662a0ea8c0f
MyximAnisimov <max170105@mail.ru>
2025-02-20 23:50:59 +0300

    add creatures for 3rd task

--- lab1/src/main/kotlin/cool_story/Creature.kt
+++ lab1/src/main/kotlin/cool_story/Creature.kt
@@ -1,4 +1,14 @@
 package itmo.tpo.cool_story
 
-class Creature {
+abstract class Creature {
+    abstract var name: String
+    abstract var location: String
+    abstract var knowledge: Int
+
+    constructor(name: String, location: String, knowledge: Int) {
+        this.name = name;
+        this.location = location;
+        this.knowledge = knowledge;
+    }
+
 }
---
+++ lab1/src/main/kotlin/cool_story/Ford.kt
@@ -0,0 +1,16 @@
+package itmo.tpo.cool_story
+
+class Ford : Creature {
+
+    override var name: String = ""
+
+    override var location: String = ""
+
+    override var knowledge: Int = 0
+
+    constructor(name: String, location: String, knowledge: Int) : super(name, location, knowledge) {
+        this.name = name
+        this.location = location
+        this.knowledge = knowledge
+    }
+}
